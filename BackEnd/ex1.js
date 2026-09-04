b283dc0f79e5ac35c48d9037f46c8605628f769b
Francisco Miguel Magri <mimagri.francisco09@gmail.com>
2026-09-04 08:26:08 -0300

    Aula 04/09/26 - Extensão MySQL no VSCode

--- BackEnd/ex1.js
+++
@@ -1,7 +0,0 @@
-let contador = 0;
-
-while (contador <=100){
-    console.log(`Contagem: ${contador}`);
-    contador +=5; // Isso aumenta 1 no contador (IMPORTANTE!)
-}
-console.log("Fim da contagem!");
